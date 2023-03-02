96967caa152cad38ebf6b55adf1f08cf5c624219
MaximilianPetzi <maximilianpetzi@gmx.de>
2023-03-02 15:30:14 -0400

    nix

--- seedavg.py
+++ seedavg.py
@@ -1,10 +1,18 @@
 #my_mosinit can also be called directly
 from termcolor import colored
 
-nA=2
-nB=5
+nA=2 #control or not
+nB=2 #seed
+nC=1
+nD=1
+nE=2
+nF=1
 stepsizeA=1 #0 for control, 1 for LTP
 stepsizeB=1002 #Seed
+stepsizeC=.4
+stepsizeD=.4
+stepsizeE=.4
+stepsizeF=.4
 
 def calcparams(ii,jj):
     pars=[None,ii,jj,ii*stepsizeA,jj*stepsizeB]  
@@ -18,17 +26,18 @@ if __name__=="__main__":
     if os.path.exists("recfolder/Data.npy"):
             os.remove("recfolder/Data.npy")
 
-    Data=np.ones((nA,nB),dtype="object")
+    Data=np.ones((nA,nB,nC,nD),dtype="object")
     np.save("recfolder/Data",Data)
-    for i in range(nA):
-        for j in range(nB):
-            myparams=calcparams(i,j) 
-
-            np.save("recfolder/myparams",myparams)  #set current params, also pass the indices for saving
-            commandstring="python2 my_mosinit.py SIMUL"
-            os.system(commandstring)    #do simulation (uses set params) (saves the recordings)
-            print(colored("after number ","red"))
-            print(i,j)
+    for a in range(nA):
+        for b in range(nB):
+            for c in range(nC):
+                for d in range(nD):
+                    myparams=calcparams(a,b,c,d) 
+                    np.save("recfolder/myparams",myparams)  #set current params, also pass the indices for saving
+                    commandstring="python2 my_mosinit.py SIMUL"
+                    os.system(commandstring)    #do simulation (uses set params) (saves the recordings)
+                    print(colored("after number ","red"))
+                    print(a,b,c,d)
     Data=np.load("recfolder/Data.npy",allow_pickle=True)
     np.save("recfolder/oldData.npy",Data)#keeps the old data until new sim is finished
 
