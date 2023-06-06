3deac9e77c4feab6bf28f6a5a501a2edf15e6717
MaximilianPetzi <maximilianpetzi@gmx.de>
2023-06-06 11:53:03 -0300

    just working on a.lagcurve for transfer entropy. preparing to plot TE for the fist time and also plot gamma power of rasterplot and soma-lfp

--- seedavg.py
+++ seedavg.py
@@ -1,28 +1,34 @@
 #runs many my_mosinit.py scripts and saves results
 #my_mosinit can also be called directly
 
-
+#for Car freq gamma test, change Car, nC, stepsizeC, calcparams.Car(cc), cpfp.Car(cc) 
+#                                and in my_mosinit: line 374 write to file a.freq()
 withspec=False
 
 nA=1 #control or not
-nB=8 #seed    #if you change the number of parameters, also change the myparams seed index in net = Network... line accordingly 
-nC=4#REC    
+nB=16 #seed    #if you change the number of parameters, also change the myparams seed index in net = Network... line accordingly 
+#nC=8#REC    
 nD=1#EXT and SOMA
-nE=4#Loc etc
+nE=2#Loc or extinsteadofrec
 stepsizeA=1 #0 for control, 1 for LTP
 stepsizeB=1002 #Seed
-stepsizeC=2.
+#stepsizeC=2.
 stepsizeD=1.
-stepsizeE=-.5 
+stepsizeE=1.
+#Car=[1,6,11,16,21,26,31,36,36.5,37,37.5,38,38.5,39,39.5,40,41,42,43] #THIS OVERWRITES stepsize
+#Car=[0,1,2,3,4,5,7,10,15]
+#Car=[15,20]
+Car=[0,1,2,6,11,16,21,26,31,36,36.5,37,37.5,38,38.5,39,39.5,40,41,42,43]
+nC=len(Car)
 #Loc goes from 1 .5 0 -.5 where -.5 changes tau1 and tau2 NMDAR as well
 
 
 #0 entry is a flag for simulation or not, indexes of parameters are given in addition to the actual parameters for easier saving:
 def calcparams(aa,bb,cc,dd,ee):#list of parameters calculated from indices
-    pars=[None,aa,bb,cc,dd,ee,1+aa*stepsizeA ,1+bb*stepsizeB+cc*177+dd*178 ,1.75+cc*stepsizeC ,1+dd*stepsizeD ,1+ee*stepsizeE ]  
+    pars=[None,aa,bb,cc,dd,ee,1+aa*stepsizeA ,1+bb*stepsizeB+cc*177+dd*178 , Car[cc],1+dd*stepsizeD ,1+ee*stepsizeE ]  #1.75+cc*stepsizeC
     return pars
 def cpfp(cc,dd,ee):#shorter list of parameters
-    return [1.75+cc*stepsizeC ,1+dd*stepsizeD ,1+ee*stepsizeE]
+    return [Car[cc] ,1+dd*stepsizeD ,1+ee*stepsizeE]
 if __name__=="__main__":
     if True:
 
