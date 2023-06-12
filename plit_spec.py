d5550ef5c6b61fc67c45c5d48d602f2f05d30c84
MaximilianPetzi <maximilianpetzi@gmx.de>
2023-06-12 07:52:03 -0300

    bigger experimen, type was the same

--- plit_spec.py
+++ plit_spec.py
@@ -18,8 +18,8 @@ for a in range(len(Data)):
             for d in range(len(Data[0,0,0])):
                 for e in range(len(Data[0,0,0,0])):     #build proper Tensor
                     Dat[a,b,c,d,e]=Data[a,b,c,d,e][1]      #full recordings, not saved anymore    
-                    Dat2[a,b,c,d,e,:]=Data[a,b,c,d,e][2:4] #theta and gamma power and potentially so much more!
-dat=np.array(Dat,dtype=float)
+                    Dat2[a,b,c,d,e,:]=(Data[a,b,c,d,e][2:4]) #theta and gamma power and potentially so much more!
+dat=np.array(Dat,dtype=float)        #(Data[a,b,c,d,e][6]["nTE_XY"],Data[a,b,c,d,e][6]["p_value_XY"])
 dat2=np.array(Dat2,dtype=float)
 imax=len(dat2[0,0])
 jmax=len(dat2[0,0,0])
