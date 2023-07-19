cc92d0fef40b0a4173301f7c2ba27bd515699c4c
MaximilianPetzi <maximilianpetzi@gmx.de>
2023-07-19 12:02:33 -0300

    nix

--- plit_spec.py
+++ plit_spec.py
@@ -7,8 +7,8 @@ import scipy.stats
 
 nrows=3
 Data=np.load("recfolder/Data.npy",allow_pickle=True)      #change back to oldData.npy
-Caro=Car[:-1]
-Data=Data[:,:,:-1]
+Caro=Car[:-3]
+Data=Data[:,:,:-3]
 
 DatShape=np.shape(Data)[0],np.shape(Data)[1],np.shape(Data)[2],np.shape(Data)[3],np.shape(Data)[4],1
 DatShape2=np.shape(Data)[0],np.shape(Data)[1],np.shape(Data)[2],np.shape(Data)[3],np.shape(Data)[4],nrows
