e9254e6a0672e5e7f8f0d40fd4aa6294de6d44e8
MaximilianPetzi <maximilianpetzi@gmx.de>
2023-08-10 12:09:14 -0300

    fix

--- geom.py
+++ geom.py
@@ -246,7 +246,6 @@ class PyrAdr(Cell):
 		
 	def set_synapses(self):
 		pww=1
-<<<<<<< HEAD
 		pf=0
 		global Location
 		global Taufac
@@ -259,18 +258,7 @@ class PyrAdr(Cell):
 		self.somaAMPAf 	 = Synapse(    sect=self.soma,   loc=0.5, tau1=0.05, tau2=5.3,e=0, 		pf=0) #this one maybe too
 		self.BdendAMPA   = Synapse(    sect=connection,  loc=Location, tau1=0.05, tau2=5.3*Taufac,e=0, 		pf=pf, pww=pww)
 		self.BdendNMDA   = SynapseNMDA(sect=self.Bdend,  loc=1, tau1=0.05, tau2=5.3,tau1NMDA=15, tau2NMDA=150, r=1, e=0)
-=======
-		pf=0#60
-		#default Loc is 1.0
-		Loc=1
-		self.somaGABAf 	 = Synapse(    sect=self.soma,   loc=0.5, tau1=0.07, tau2=9.1,e=-80, 	pf=0)#tau1=0.07, tau2=9.1
-		self.somaAMPAf 	 = Synapse(    sect=self.soma,   loc=0.5, tau1=0.05, tau2=5.3,e=0, 		pf=0) #this one maybe too
-		self.BdendAMPA   = Synapse(    sect=self.Bdend,  loc=1, tau1=0.05, tau2=5.3*Loc,e=0, 		pf=pf, pww=pww)
-		self.BdendNMDA   = SynapseNMDA(sect=self.Bdend,  loc=1, tau1=0.05, tau2=5.3,tau1NMDA=15, tau2NMDA=150, r=1, e=0, pww=1)
-<<<<<<< HEAD
->>>>>>> somasynch
-=======
->>>>>>> somasynch
+
 		self.Adend2GABAs = Synapse(	   sect=self.Adend2, loc=0.5, tau1=0.2,  tau2=20,e=-80, 	pf=0)
 		self.Adend3GABAf = Synapse(	   sect=self.Adend3, loc=0.5, tau1=0.07, tau2=9.1,e=-80, 	pf=0)
 		self.Adend3AMPAf = Synapseb(	   sect=self.Adend3, loc=0.5, tau1=0.05, tau2=5.3,e=0, 		pf=pf, pww=pww)#likely, LTP happens here
